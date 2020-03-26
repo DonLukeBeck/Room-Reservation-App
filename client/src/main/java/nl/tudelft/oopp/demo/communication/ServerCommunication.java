6ff807b69d5f0dce672ea96d05daec8729309eba
Luca Becheanu <l.becheanu@student.tudelft.nl>
2020-03-26 18:55:38 +0100

    fixed server communication again

--- client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
+++ client/src/main/java/nl/tudelft/oopp/demo/communication/ServerCommunication.java
@@ -3,7 +3,13 @@ package nl.tudelft.oopp.demo.communication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
-import nl.tudelft.oopp.demo.entities.*;
+import nl.tudelft.oopp.demo.entities.Buildings;
+import nl.tudelft.oopp.demo.entities.Dishes;
+import nl.tudelft.oopp.demo.entities.Holidays;
+import nl.tudelft.oopp.demo.entities.Menus;
+import nl.tudelft.oopp.demo.entities.Reservations;
+import nl.tudelft.oopp.demo.entities.Rooms;
+import nl.tudelft.oopp.demo.entities.Users;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -26,7 +32,9 @@ public class ServerCommunication {
      */
     public boolean signUp(String user, String pass, String role) {
 
-        String body = "{\"netid\":\"" + user + "\",\"password\":\"" + pass + "\",\"role\":\"" + role + "\"}";
+        String body = "{\"netid\":\"" + user + "\",\"password\":\""
+                + pass + "\",\"role\":\"" + role + "\"}";
+
 
         try {
             boolean bool = this.webClient.post().uri("/registerNewUser")
@@ -50,9 +58,11 @@ public class ServerCommunication {
     }
 
     /**
-     * @param user
-     * @param pass
-     * @return
+     * Sends log in credentials to the server.
+     *
+     * @param user net_id
+     * @param pass password from the user
+     * @return true if the user is in the database, false if not.
      */
     public Users logIn(String user, String pass) {
 
@@ -111,8 +121,12 @@ public class ServerCommunication {
                 .block();
 
         ObjectMapper mapper = new ObjectMapper();
-        List<Buildings> buildingsJsonList = mapper.readValue(jsonString, new com.fasterxml.jackson.core.type.TypeReference<List<Buildings>>() {
-        });
+        List<Buildings> buildingsJsonList = mapper
+                .readValue(jsonString, new com.fasterxml
+                        .jackson
+                        .core
+                        .type
+                        .TypeReference<List<Buildings>>() {});
 
         return buildingsJsonList;
     }
@@ -138,8 +152,14 @@ public class ServerCommunication {
                 .block();
 
         ObjectMapper mapper = new ObjectMapper();
-        List<Rooms> roomsJsonList = mapper.readValue(jsonString, new com.fasterxml.jackson.core.type.TypeReference<List<Rooms>>() {
-        });
+        List<Rooms> roomsJsonList = mapper
+                .readValue(jsonString, new com
+                        .fasterxml
+                        .jackson
+                        .core
+                        .type
+                        .TypeReference<List<Rooms>>() {});
+
 
         return roomsJsonList;
     }
@@ -166,8 +186,14 @@ public class ServerCommunication {
                 .block();
 
         ObjectMapper mapper = new ObjectMapper();
-        List<Reservations> reservationsJsonList = mapper.readValue(jsonString, new com.fasterxml.jackson.core.type.TypeReference<List<Reservations>>() {
-        });
+        List<Reservations> reservationsJsonList = mapper
+                .readValue(jsonString, new com
+                        .fasterxml
+                        .jackson
+                        .core
+                        .type
+                        .TypeReference<List<Reservations>>() {});
+
 
         return reservationsJsonList;
     }
@@ -194,8 +220,14 @@ public class ServerCommunication {
                 .block();
 
         ObjectMapper mapper = new ObjectMapper();
-        List<Menus> menusJsonList = mapper.readValue(jsonString, new com.fasterxml.jackson.core.type.TypeReference<List<Menus>>() {
-        });
+        List<Menus> menusJsonList = mapper
+                .readValue(jsonString, new com
+                        .fasterxml
+                        .jackson
+                        .core
+                        .type
+                        .TypeReference<List<Menus>>() {});
+
 
         return menusJsonList;
     }
@@ -221,8 +253,13 @@ public class ServerCommunication {
                 .block();
 
         ObjectMapper mapper = new ObjectMapper();
-        List<Holidays> holidaysJsonList = mapper.readValue(jsonString, new com.fasterxml.jackson.core.type.TypeReference<List<Holidays>>() {
-        });
+        List<Holidays> holidaysJsonList = mapper
+                .readValue(jsonString, new com
+                        .fasterxml
+                        .jackson
+                        .core
+                        .type
+                        .TypeReference<List<Holidays>>() {});
 
         return holidaysJsonList;
     }
@@ -248,8 +285,14 @@ public class ServerCommunication {
                 .block();
 
         ObjectMapper mapper = new ObjectMapper();
-        List<Dishes> dishesJsonList = mapper.readValue(jsonString, new com.fasterxml.jackson.core.type.TypeReference<List<Dishes>>() {
-        });
+        List<Dishes> dishesJsonList = mapper
+                .readValue(jsonString, new com
+                        .fasterxml
+                        .jackson
+                        .core
+                        .type
+                        .TypeReference<List<Dishes>>() {});
+
 
         return dishesJsonList;
     }
@@ -259,10 +302,18 @@ public class ServerCommunication {
      *
      * @return true if new user is created, false if not.
      */
-    public boolean reservation(String userReserving, String timeSlot, String date, int buildingReserved, String room) {
+    public boolean reservation(String userReserving,
+                               String timeSlot,
+                               String date,
+                               int buildingReserved,
+                               String room) {
+
         System.out.println(userReserving);
         System.out.println(room);
-        String body = "{\"userReserving\":\"" + userReserving + "\",\"timeslot\":\"" + timeSlot + "\",\"date\":\"" + date + "\",\"buildingReserved\":\"" + buildingReserved + "\",\"roomReserved\":\"" + room + "\"}";
+        String body = "{\"userReserving\":\""
+                + userReserving + "\",\"timeslot\":\""
+                + timeSlot + "\",\"date\":\"" + date + "\",\"buildingReserved\":\""
+                + buildingReserved + "\",\"roomReserved\":\"" + room + "\"}";
 
         try {
             boolean bool = this.webClient.post().uri("/postReservation")
@@ -286,18 +337,33 @@ public class ServerCommunication {
     }
 
     /**
-     * @param buildingID
-     * @param buildingName
-     * @param buildingOpen
-     * @param buildingClose
-     * @param imageUrl
-     * @param bikeCapacity
-     * @param roomCapacity
-     * @return
+     * Adds the given building to the database.
+     *
+     * @param buildingID buildingID
+     * @param buildingName buildingName
+     * @param buildingOpen building Opening Hours
+     * @param buildingClose building Closing Hours
+     * @param imageUrl imageUrl
+     * @param bikeCapacity bikeCapacity
+     * @param roomCapacity roomCapacity
+     * @return true if building successfully added, false otherwise.
      */
-    public boolean addBuildingAdmin(int buildingID, String buildingName, String buildingOpen, String buildingClose, String imageUrl, int bikeCapacity, int roomCapacity) {
+    public boolean addBuildingAdmin(int buildingID,
+                                    String buildingName,
+                                    String buildingOpen,
+                                    String buildingClose,
+                                    String imageUrl,
+                                    int bikeCapacity,
+                                    int roomCapacity) {
 
-        String body = "{\"buildingNumber\":\"" + buildingID + "\",\"name\":\"" + buildingName + "\",\"openingHours\":\"" + buildingOpen + "\",\"closingHours\":\"" + buildingClose + "\",\"numberOfRooms\":\"" + roomCapacity + "\",\"numberOfBikes\":\"" + bikeCapacity + "\",\"url\":\"" + imageUrl + "\"}";
+        String body = "{\"buildingNumber\":\"" + buildingID
+                + "\",\"name\":\"" + buildingName
+                + "\",\"openingHours\":\"" + buildingOpen
+                + "\",\"closingHours\":\"" + buildingClose
+                + "\",\"numberOfRooms\":\"" + roomCapacity
+                + "\",\"numberOfBikes\":\"" + bikeCapacity
+                + "\",\"url\":\"" + imageUrl + "\"}";
+
         System.out.println(body);
         try {
             boolean bool = this.webClient.post().uri("/addBuilding")
@@ -321,15 +387,20 @@ public class ServerCommunication {
     }
 
     /**
-     * @param roomID
-     * @param roomCap
-     * @param buildingID
-     * @param roomType
-     * @return
+     * Adds the given room to the database.
+     *
+     * @param roomID roomID
+     * @param roomCap roomCap
+     * @param buildingID buildingID
+     * @param roomType roomType
+     * @return true if building successfully added, false otherwise.
      */
     public boolean addRoomAdmin(String roomID, int roomCap, int buildingID, String roomType) {
 
-        String body = "{\"roomId\":\"" + roomID + "\",\"capacity\":\"" + roomCap + "\",\"type\":\"" + roomType + "\",\"associatedBuilding\":\"" + buildingID + "\"}";
+        String body = "{\"roomId\":\"" + roomID
+                + "\",\"capacity\":\"" + roomCap
+                + "\",\"type\":\"" + roomType
+                + "\",\"associatedBuilding\":\"" + buildingID + "\"}";
         System.out.println(body);
         try {
             boolean bool = this.webClient.post().uri("/addRoom")
