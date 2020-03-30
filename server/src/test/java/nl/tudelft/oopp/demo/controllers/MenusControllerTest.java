48282fc2c9d72857f5b995094c8c94db4606c958
François Ezard <Ezard-1@student.tudelft.nl>
2020-03-30 22:33:50 +0200

    100% Coverage MenusController

--- server/src/test/java/nl/tudelft/oopp/demo/controllers/MenusControllerTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/controllers/MenusControllerTest.java
@@ -1,6 +1,6 @@
 package nl.tudelft.oopp.demo.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
@@ -43,5 +43,58 @@ public class MenusControllerTest {
 
     }
 
+    @Test
+    public void addMenuFalseTest() {
+        m1 = new Menus();
+        m1.setDishName("m1");
+        when(menusRepository.findMenu(m1.getBuildingNumber(), m1.getDishName())).thenReturn(m1);
+        assertFalse(menusController.addMenu(m1));
+    }
+
+    @Test
+    public void addMenuFalseCase2Test() {
+        m1 = new Menus();
+        m1.setDishName("");
+        when(menusRepository.findMenu(m1.getBuildingNumber(), m1.getDishName())).thenReturn(m1);
+        assertFalse(menusController.addMenu(m1));
+    }
+
+    @Test
+    public void addMenuTrueTest() {
+        m1 = new Menus();
+        assertTrue(menusController.addMenu(m1));
+    }
+
+    @Test
+    public void editMenuTrueTest() {
+        m1 = new Menus();
+
+        when(menusRepository.updateExistingMenu(m1.getBuildingNumber(), m1.getDishName(), "d1")).thenReturn(1);
+        assertTrue(menusController.editMenu(m1, "d1"));
+    }
+
+    @Test
+    public void editMenuFalseTest() {
+        m1 = new Menus();
+
+        when(menusRepository.updateExistingMenu(m1.getBuildingNumber(), m1.getDishName(), "d1")).thenReturn(2);
+        assertFalse(menusController.editMenu(m1, "d1"));
+    }
+
+    @Test
+    public void editMenuFalseCase2Test() {
+        m1 = new Menus();
+
+        when(menusRepository.updateExistingMenu(m1.getBuildingNumber(), m1.getDishName(), "d1")).thenThrow(NullPointerException.class);
+        assertFalse(menusController.editMenu(m1, "d1"));
+    }
+
+    @Test
+    public void deleteMenuTest() {
+        m1 = new Menus();
+
+        when(menusRepository.deleteMenu(m1.getBuildingNumber(), m1.getDishName())).thenReturn(true);
+        assertTrue(menusController.deleteMenu(m1.getBuildingNumber(), m1.getDishName()));
+    }
 
 }
