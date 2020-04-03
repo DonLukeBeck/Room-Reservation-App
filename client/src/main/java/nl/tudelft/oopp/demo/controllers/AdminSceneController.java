e6c5a71180707474141d6f0b26420feff7f0b438
Luca Andreoli <l.andreoli@student.tudelft.nl>
2020-04-03 17:51:16 +0200

    new methods for new pages

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminSceneController.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/AdminSceneController.java
@@ -26,8 +26,13 @@ public class AdminSceneController {
      * @param event Logging in as admin
      * @throws IOException Exception if can't find admin view scene
      */
-    public void goToAdminAdd(ActionEvent event) throws IOException {
+    public void goToAdminAddSelection(ActionEvent event) throws IOException {
 
+        HelperController helperController = new HelperController();
+        helperController.loadNextScene("/AddSelectionAdmin.fxml", mainScreen);
+    }
+
+    public void goToAdminAdd(ActionEvent event) throws IOException {
         HelperController helperController = new HelperController();
         helperController.loadNextScene("/AdminView.fxml", mainScreen);
     }
@@ -41,5 +46,17 @@ public class AdminSceneController {
 
         HelperController helperController = new HelperController();
         helperController.loadNextScene("/AdminEditView.fxml", mainScreen);
+    }
+
+    public void goToAdminHolidayAdd(ActionEvent event) throws IOException {
+
+        HelperController helperController = new HelperController();
+        helperController.loadNextScene("/AdminHolidayAdd.fxml", mainScreen);
+    }
+
+    public void goBack(ActionEvent event) throws IOException {
+        HelperController helperController = new HelperController();
+        helperController.loadNextScene("/MainAdminScene.fxml", mainScreen);
+
     }
 }
