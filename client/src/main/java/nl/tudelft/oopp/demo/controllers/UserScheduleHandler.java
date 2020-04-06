391a60b05e0f184fee8d109c99ce0bb8a62863a8
amaurycharlot <a.c.a.charlot@student.tudelft.nl>
2020-04-06 14:23:41 +0200

    Fixed bug when opening a day view in schedule.

--- client/src/main/java/nl/tudelft/oopp/demo/controllers/UserScheduleHandler.java
+++ client/src/main/java/nl/tudelft/oopp/demo/controllers/UserScheduleHandler.java
@@ -103,6 +103,7 @@ public class UserScheduleHandler implements javafx.event.EventHandler<MouseEvent
         FXMLLoader loader = new FXMLLoader();
         URL xmlUrl = getClass().getResource("/UserScheduleDayView.fxml");
         loader.setLocation(xmlUrl);
+        Parent root = loader.load();
         UserScheduleDayView controller = loader.<UserScheduleDayView>getController();
         controller.setDay(day);
         controller.setMonth(month);
@@ -111,8 +112,6 @@ public class UserScheduleHandler implements javafx.event.EventHandler<MouseEvent
         controller.setUserEvents(userEvents);
         controller.setEventsText(eventsText);
         controller.initialize();
-
-        Parent root = loader.load();
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.getIcons().add(new Image("images/favicon.png"));
